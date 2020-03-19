8beb1eefb7ac170224c7efed547ba59096d73a87
Sebastian Franzén <sebastian@Sebastians-MacBook-Pro.local>
2020-03-19 20:44:31 +0100

    Ready for pull request

--- travelup/src/components/BrowseFood.js
+++ travelup/src/components/BrowseFood.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {ENDPOINT, API_KEY} from "../apiConfig.js";
+const location_id = "189852"
 
 class BrowseFood extends React.Component {
   constructor(props) {
@@ -10,7 +11,7 @@ class BrowseFood extends React.Component {
   }
 
   componentWillMount(){
-    fetch(ENDPOINT+"list?restaurant_tagcategory_standalone=10591&lunit=km&restaurant_tagcategory=10591&limit=30&prices_restaurants=10953%252C10955&restaurant_mealtype=10598%252C10599&currency=USD&lang=en_US&location_id=293919", {
+    fetch(ENDPOINT+"list?restaurant_tagcategory_standalone=10591&lunit=km&restaurant_tagcategory=10591&limit=30&prices_restaurants=10953%252C10955&restaurant_mealtype=10598%252C10599&currency=SEK&lang=en_US&location_id="+location_id, {
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
@@ -30,20 +31,12 @@ class BrowseFood extends React.Component {
         <h5>Price Range: {restaurant.price} </h5>
         {restaurant.is_closed ? <h5>Closed</h5> : 
           <h5>{restaurant.open_now_text}</h5>}
-          <br />
+        <p>Address: {restaurant.address} </p>
+        <br />
       </div>
     ));
     return (
       <div>
-        <nav className="nav-wrapper red darken-3">
-          <div className="container">
-            <a className="brand-logo">TravelUp</a>
-            <ul className="right">
-              <li><a href="/">Login</a></li>
-              <li><a href="/food">Food</a></li> 
-            </ul> 
-          </div>
-        </nav>
         <h1>Restaurants</h1>
         {restaurantItems}
       </div>
