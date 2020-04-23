085e50d97c8b304cd083732b55188d001ae21e05
stinalangstrom <stinalangstrom@gmail.com>
2020-04-23 13:48:23 +0200

    reset everything when user logs out #28

--- travelup/src/components/SignedInLinks.js
+++ travelup/src/components/SignedInLinks.js
@@ -3,11 +3,23 @@ import { Link } from "react-router-dom"
 import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 import { connect } from "react-redux";
 import { signOut } from '../actions/authActions'
+import { resetTrip } from '../actions/tripActions'
+import { resetLocation } from '../actions/plannerActions'
 
 const lnkStyle = {
   color: 'white',
   textDecoration: 'none',
   position:'left'
+}
+
+// When we sign out we need to reset everything we have started on
+const signOutFunc = (props) => {
+  if(window.confirm("If you sign out now your changes will be discarded. Do you still wan't to sign out?")){
+    props.resetTrip();
+    props.resetLocation();
+    props.signOut();
+  } // else, do nothing
+  
 }
 
 const SignedInLinks = (props) => {
@@ -17,7 +29,7 @@ const SignedInLinks = (props) => {
         <Button color="inherit"><Link to="/planner" style={lnkStyle} activeStyle={lnkStyle}>Planner</Link></Button>
         <Button color="inherit"><Link to="/select" style={lnkStyle} activeStyle={lnkStyle}>Select</Link></Button>
         <Button color="inherit"><Link to="/allTrips" style={lnkStyle} activeStyle={lnkStyle}>All Trips</Link></Button>
-        <Button color="inherit" style={{ position:'right' }} onClick={() => { props.signOut(); }}>Sign Out</Button>
+        <Button color="inherit" style={{ position:'right' }} onClick={() => { signOutFunc(props); }}>Sign Out</Button>
       </ul>
     </div>
 
@@ -27,6 +39,8 @@ const SignedInLinks = (props) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     signOut: () => dispatch(signOut()),
+    resetTrip: () => dispatch(resetTrip()),
+    resetLocation: () => dispatch(resetLocation()),
 
   }
 }
