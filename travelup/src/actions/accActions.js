c297322624324d1abe2da560e5c4da2f7d5acff6
Sebastian Franzén <franzen@Frazzes-Macbook.local>
2020-05-04 00:20:53 +0200

    Added fetch for accommodation

--- travelup/src/actions/accActions.js
+++ travelup/src/actions/accActions.js
@@ -1,10 +1,10 @@
 import { FETCH_ACCOMMODATIONS, RESET_ACCOMMODATIONS } from "./types";
 import {ENDPOINT, API_KEY} from "../apiConfig";
 
-export const fetchAcc = (location_id, activity_subcategory = "0", activity_rating = "all") => dispatch => {
+export const fetchAcc = (location_id, guests = "2", rooms = "1") => dispatch => {
   // reset the restaurants before fetching new ones, this is so old values are not shown after a new search has been made
   dispatch({ type: RESET_ACCOMMODATIONS });
-  fetch(ENDPOINT+"attractions/list?lang=en_US&currency=USD&sort=recommended&lunit=km&limit=30&bookable_first=false&subcategory="+activity_subcategory+"&rating="+activity_rating+"&location_id="+location_id, {
+  fetch(ENDPOINT+"hotels/list?offset=0&currency=USD&limit=30&order=asc&lang=en_US&sort=recommended&nights=2&location_id="+location_id+"&adults="+guests+"&rooms="+rooms, {
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
