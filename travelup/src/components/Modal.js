c2b725f3a1c80501d9a9479548f29c17f6e86213
Cecilia Mikiver <ceciliamikiver@Cecilias-MacBook-Air.local>
2020-05-06 09:28:01 +0200

    Revert "modal #39"
    This reverts commit f3fc510100adbc1baea9994fd2b6373ca18324a6.

--- travelup/src/components/Modal.js
+++ travelup/src/components/Modal.js
@@ -4,6 +4,10 @@ import { Button} from '@material-ui/core';
 export default class Modal extends React.Component {
     
 
+// onClose = e => {
+//     this.props.onClose && this.props.onClose(e);
+//     };
+
   render() {
     console.log(this.props.show)
     console.log(this.props.data)
@@ -11,38 +15,17 @@ export default class Modal extends React.Component {
       return null;
     }
 
-    let website = ""
-    if(this.props.data.website){
-        website = "Click here to find out more"
-    }
-
-    let modalDiv;
-    if(this.props.type === "o"){
-        modalDiv = (
-            <div>
-            <div className="modal_title">{this.props.data.name}</div>
-              <img src={this.props.data.photo}/>
-              <div className="modal_text">{this.props.data.description}</div>
-              <div className="price_text">{this.props.data.price}</div>
-              <p><a href={this.props.data.website}>{website}</a></p>
-              </div>
-            );
-    }
-    else{
-        modalDiv = (
-            <div>
-            <div className="modal_title">{this.props.data.name}</div>
-              <img src={this.props.data.photo}/>
-              <div className="price_text">{this.props.data.price}</div>
-            </div>
-        )
-    }
-
-
     return (
         <div className="modal">
-            {modalDiv}
-          <div><button className="modal_btn" variant="outlined" onClick={this.props.onClose}>Close</button></div>
+          <div className="modal_title">{this.props.data.name}</div>
+          <img src={this.props.data.photo}/>
+          <div className="modal_text">{this.props.data.description}</div>
+          <div className="price_text">{this.props.data.price}</div>
+          <div><button className="modal_btn" variant="outlined" onClick={this.props.onClose}>
+              Close
+            </button></div>
+          <div>
+          </div>
         </div>
       );
   }
