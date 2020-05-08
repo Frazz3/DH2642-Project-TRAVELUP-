11c74b7d646a8288b141d919cb66c243e67b511b
Sebastian Franzén <franzen@Frazzes-Macbook.local>
2020-05-08 13:59:24 +0200

    Changed currency in restaurants

--- travelup/src/actions/foodActions.js
+++ travelup/src/actions/foodActions.js
@@ -4,7 +4,7 @@ import {ENDPOINT, API_KEY} from "../apiConfig";
 export const fetchRestaurants = (location_id, restaurant_mealtype = "all", prices_restaurants = "all") => dispatch => {
   // reset the restaurants before fetching new ones, this is so old values are not shown after a new search has been made 
   dispatch({ type: RESET_RESTAURANTS });
-  fetch(ENDPOINT+"restaurants/list?restaurant_tagcategory_standalone=10591&lunit=km&restaurant_tagcategory=10591&limit=30&prices_restaurants="+prices_restaurants+"&restaurant_mealtype="+restaurant_mealtype+"&currency=SEK&lang=en_US&location_id="+location_id, {
+  fetch(ENDPOINT+"restaurants/list?restaurant_tagcategory_standalone=10591&lunit=km&restaurant_tagcategory=10591&limit=30&prices_restaurants="+prices_restaurants+"&restaurant_mealtype="+restaurant_mealtype+"&currency=USD&lang=en_US&location_id="+location_id, {
       "method": "GET",
       "headers": {
         "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
